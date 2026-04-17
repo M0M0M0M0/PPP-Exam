f100188620f0388910674f5b2c604e97113a7cba
quocdang <dangvanquocmomo@gmail.com>
2026-04-17 18:50:55 +0700

    add

---
+++ InheritanceAndPolymorphism/Program.cs
@@ -0,0 +1,13 @@
+﻿using Inheritance;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        Tiger tiger = new Tiger(100, "tiger");
+        tiger.Show();
+
+        Lion lion = new Lion(200, "lion");
+        lion.Show();
+    }
+}
